49ce4bc14c8207688bf93f088307806180e29f09
Gerwin Verin <gerwinverin@gmail.com>
2026-08-12 12:59:41 +0700

    commit

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
-const credentials = require("./key.json");
+// const credentials = require("./key.json");
 // const credentials = require(process.env.FIREBASE_SERVICE_ACCOUNT);
-// const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT || "{}";
-// const credentials = JSON.parse(serviceAccountString);
+const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT || "{}";
+const credentials = JSON.parse(serviceAccountString);
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials)
