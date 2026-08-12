2c5f575b7dfafc8994a89e7ef69040b3972bd561
Gerwin Verin <gerwinverin@gmail.com>
2026-08-12 20:45:10 +0700

    commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const userRoutes = require("users");
-const rsvpRoutes = require("rsvp");
+const userRoutes = require("./users");
+const rsvpRoutes = require("./rsvp");
 const cors = require('cors');
 
 app.use(cors());
